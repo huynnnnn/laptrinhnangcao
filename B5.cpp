7e0eeb63562fb2b0f186c76a94dfad4520c0bdb8
huynnnnn <100114998+huynnnnn@users.noreply.github.com>
2022-02-21 14:05:16 +0700

    Create B5.cpp

---
+++ B5.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double c, f = 0;
+    cin >> c;
+    f = double(32) + c*double(9)/5;
+    cout << fixed << setprecision(2) << f;
+    return 0;
+}
